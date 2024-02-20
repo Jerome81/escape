53c3d89fad39afb91d52fbbc8269747d0d7364f2
unknown <jeromemueller81@gmail.com>
2024-02-20 11:31:16 +0100

    Enter homebase IP and device name on startup

---
+++ homebase/central.py
@@ -0,0 +1,67 @@
+from flask import Flask, request, jsonify, send_file
+from sqlitedict import SqliteDict
+from datetime import date
+from config import get_config
+import shutil
+import os
+
+
+central = Flask(__name__)
+content_dir = get_config()["content_dir"]
+print(content_dir)
+
+@central.get("/outposts")
+def get_outposts(cache_file="outposts.sqlite3"):
+    try:
+        all = { "test": 1 }
+        with SqliteDict(cache_file) as outposts:
+            for key in outposts:
+                all[key] = outposts[key]
+
+        return jsonify(all)
+    except Exception as ex:
+        print("Error while loading outposts data:", ex)
+        return "Error"
+    
+@central.post("/outpost")
+def add_outpost():
+    if request.is_json:
+        outpost = request.get_json()
+        print(outpost)
+        outpost["last_seen"] = date.today().strftime("%d.%m.%Y")
+        save(outpost["name"], outpost)
+        return outpost, 201
+    return {"error": "Request must be JSON"}, 415
+
+def save(key, value, cache_file="outposts.sqlite3"):
+    print("Signal received from outpost. Saving connection data.")
+    print(value)
+    try:
+        with SqliteDict(cache_file) as outposts:
+            outposts[key] = value 
+            outposts.commit() 
+    except Exception as ex:
+        print("Error while storing data (Possibly unsupported):", ex)
+
+@central.get("/outpost")
+def get_outpost():
+    return jsonify(find_outpost(request.args["id"]))
+ 
+def find_outpost(key, cache_file="outposts.sqlite3"):
+    try:
+        with SqliteDict(cache_file) as outposts:
+            value = outposts[key] # No need to use commit(), since we are only loading data!
+        return value
+    except Exception as ex:
+        print("Error while loading data:", ex)
+
+
+@central.get("/content")
+def get_content():
+    id = request.args["id"]
+    folder = content_dir + id
+    print("Requested content from: " + folder)
+    
+#    os.remove(temp_file)
+    shutil.make_archive(folder, 'zip', folder)
+    return send_file(folder + ".zip")
