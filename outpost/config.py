c69ee1e55e6607b64226c41e29bb96f7df42005e
Jerome <jeromemueller81@gmail.com>
2024-02-22 21:10:27 +0100

    Fix path issue and change directory to scripts

--- outpost/config.py
+++ outpost/config.py
@@ -4,18 +4,18 @@ import json
 
 def get_config():
     script_location = os.path.dirname(os.path.realpath(__file__))
-    if not os.path.exists(script_location + '\config.json'):
-        with open(script_location + '\config.orig') as json_file:
+    if not os.path.exists(script_location + '/config.json'):
+        with open(script_location + '/config.orig') as json_file:
              print("Loading original configs")
              data = json.load(json_file)
              print(data)
              data["name"] = input("Name of device: ")
              data["api_url"] = input("API URL (e.g. http://127.0.0.1:5000/)")
-             with open(script_location + '\config.json', 'w') as new_file:
+             with open(script_location + '/config.json', 'w') as new_file:
                  print("Writing new config file: %s" % data)
                  json.dump(data, new_file)
      
-    with open(script_location + '\config.json') as json_file:
+    with open(script_location + '/config.json') as json_file:
         data = json.load(json_file)
 
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
--- outpost/initializing.py
+++ outpost/initializing.py
@@ -3,6 +3,7 @@ from status import set_status
 
 def initializing(outpost_def):
     set_status("Initializing", outpost_def)
+    os.chdir(outpost_def["script_location"])
     content_dir = outpost_def["content_dir"]
     if not os.path.isdir(content_dir):
         os.makedirs(content_dir, exist_ok=True)
