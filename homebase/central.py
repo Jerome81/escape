feb00b4ee6a6e7420962d0e891f8d88ea623336d
unknown <jeromemueller81@gmail.com>
2024-03-06 19:35:32 +0100

    Add outposts overview page and remove old entries

--- homebase/central.py
+++ homebase/central.py
@@ -5,30 +5,112 @@ from config import get_config
 import shutil
 import os
 
-
+DATE_FORMAT = "%d.%m.%Y - %H:%M:%S"
 central = Flask(__name__)
 content_dir = get_config()["content_dir"]
 print(content_dir)
 
-@central.get("/outposts")
-def get_outposts(cache_file="outposts.sqlite3"):
+# Removing old entries
+with SqliteDict("outposts.sqlite3") as outposts:
+    for key in outposts:
+        o = outposts[key]
+        try:
+            print(o["last_seen"])
+            last_seen = datetime.strptime(o["last_seen"], DATE_FORMAT)
+            print(datetime.now() - last_seen)
+            if ((datetime.now() - last_seen).days > 7):
+                print("Removing outpost, because we didn't have contact for more than 7 days.")
+                del outposts[key]
+                outposts.commit()
+
+        except Exception as ex:
+            print(ex)
+            print("Removing the following outpost, because I can't determine when we last had contact: %s" % o)
+            del outposts[key]
+            outposts.commit()
+
+
+@central.get("/outpostsjson")
+def get_outposts_json(cache_file="outposts.sqlite3"):
     try:
         all = {}
         with SqliteDict(cache_file) as outposts:
             for key in outposts:
                 all[key] = outposts[key]
-
+               
+        
         return jsonify(all)
     except Exception as ex:
         print("Error while loading outposts data:", ex)
         return "Error"
+
+@central.get("/outposts")
+def get_outposts(cache_file="outposts.sqlite3"):
+    try:
+        all = {}
+        page = """<html><head>
+        <link rel='stylesheet' href='/static/styles.css'/>
+        <script src="/static/jquery-3.6.0.min.js"></script>
+        <script>
+        function send_request(type, ip, method, resultDiv)
+        {
+            $.ajax({
+
+                type: type,
+                url: 'http://' + ip + '/' + method,
+                success: function(data) {
+                    $('#' + resultDiv).html(data);
+                },
+                error: function(data) {
+                    $('#' + resultDiv).html(data);
+                }
+            });
+
+        }
+        </script>
+        </head>
+        <body>%s</body>
+        </html>"""
+        cards = ""
+        i = 0
+        with SqliteDict(cache_file) as outposts:
+            for key in outposts:
+                i = i + 1
+                o = outposts[key]
+                all[key] = outposts[key]
+                c = """<div class='card'>
+                <div class='title'>%s</div>
+                <div class='status'><b>Status:</b><br/>%s</div>
+                <div class='actions'>%s</div>
+                <div class='result' id='result""" + str(i) + """'></div>
+                </div>"""
+                cards = cards + (c % (o["name"] + " - " + o["last_seen"], o["status"], get_actions(o, i, o["IP"])))
+
+        
+        return (page % cards)
+    except Exception as ex:
+        print("Error while loading outposts data:", ex)
+        return "Error"
     
+def get_actions(outpost, number, ip):
+    
+    resultDiv = "result" + str(number)
+    action_template = "<a onclick=\"send_request('%s', '%s', '%s', '" + resultDiv + "')\" class='button' >%s</a>"
+    actions = ""
+    actions = actions + action_template % ("GET", ip + ":5001", "status", "Boot Status")
+    actions = actions + action_template % ("GET", ip + ":5001", "download", "Download")
+    actions = actions + action_template % ("GET", ip + ":5002", "attraction_mode", "Attraction mode")
+    actions = actions + action_template % ("GET", ip + ":5002", "stats", "Statistics")
+    actions = actions + action_template % ("GET", ip + ":5002", "status", "Status")
+    return actions
+
+
 @central.post("/outpost")
 def add_outpost():
     if request.is_json:
         outpost = request.get_json()
         print(outpost)
-        outpost["last_seen"] = datetime.now().strftime("%d.%m.%Y - %H:%M:%S")
+        outpost["last_seen"] = datetime.now().strftime(DATE_FORMAT)
         save(outpost["name"], outpost)
         return outpost, 201
     return {"error": "Request must be JSON"}, 415
