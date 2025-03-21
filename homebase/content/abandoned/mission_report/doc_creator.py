42c3286abc0a51df4383f1116c38f83f250d2b2c
Game O'Rama <jeromemueller81@gmail.com>
2025-03-21 12:34:38 +0100

    Missionreport creator, basic version, multi language

--- homebase/content/abandoned/mission_report/doc_creator.py
+++ homebase/content/abandoned/mission_report/doc_creator.py
@@ -1,6 +1,9 @@
 import paho.mqtt.client as mqtt
 import json
+import webbrowser
 from time import sleep
+from datetime import datetime
+from datetime import timedelta
 
 
 import os.path
@@ -16,31 +19,119 @@ from googleapiclient.errors import HttpError
 SCOPES = ["https://www.googleapis.com/auth/documents", "https://www.googleapis.com/auth/drive"]
 
 # The ID of a sample document.
-DOCUMENT_ID = "13Pzz7txy-8j__LoPIHu55LGJe_C-2miZvtY-eeqvqlw"
+DOCUMENT_ID = "13Pzz7txy-8j__LoPIHu55LGJe_C-2miZvtY-eeqvqlw"  # https://docs.google.com/document/d/13Pzz7txy-8j__LoPIHu55LGJe_C-2miZvtY-eeqvqlw/edit?tab=t.0
 
 # The ID of the folder into which the new doc is created
-FOLDER_ID = '1ohugy5OyUwvX2D7UWaa9_8oOp8T65c6e'
+FOLDER_ID = '1ohugy5OyUwvX2D7UWaa9_8oOp8T65c6e'  # https://drive.google.com/drive/folders/1ohugy5OyUwvX2D7UWaa9_8oOp8T65c6e
 
-mq_ip = "192.168.178.11"
+mq_ip = "10.134.178.101"
 
 deviceId = "DocWriter"
+
+docs_service = None
+doc_id = None
 
 _jsonData = {
     "id": deviceId,
     "description": ("Keine Lösung"),
 }
 
-_language = "de"
+_language = "deutsch"
+_score = 0
+
+MISSION_STARTED = {
+    "deutsch": "%s - Mission start",
+    "english": "%s = Mission started"
+}
+
+ENTERED_SPACESHIP = {
+    "deutsch": "%s - Das verlassene Raumschiff betreten",
+    "english": "%s - Entered the abandoned spaceship"
+}
+
+DOCK_KEYPAD_SOLVED = {
+    "deutsch": "%s - Zutrittscode geknackt",
+    "english": "%s - Access code cracked"
+}
+
+DOCK_DOOR_UNLOCKED = {
+    "deutsch": "%s - Dock Türe geöffnet",
+    "english": "%s - Dock door opened"
+}
+
+INTRUDER_ALERT_SUCCESS = {
+    "deutsch": "Eindringlingsalarm erfolgreich ausgeschaltet",
+    "english": "Intruder alert successfully avoided"
+}
+
+INTRUDER_ALERT_FAILED = {
+    "deutsch": "Eindringlingsalarm ausgelöst\t",
+    "english": "Intruder alert triggered"
+}
+
+QUICKNESS_BONUS = {
+    "deutsch": "Geschwindigkeitsbonus",
+    "english": "Bonus for speed"    
+}
+
+
+
+_start_time = None
+
+
+def add_to_log(text):
+    Logtag = "#LOGBUCH"
+    replace_text(Logtag, text + "\n" + Logtag)
+
+
+def add_score(text):
+    tag = "#SCORE"
+    replace_text(tag, text + "\n" + tag)
+
+
+def add_to_log_with_time(text):
+    add_to_log(text % datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+
+def add_to_score(score, text):
+    global _score
+    _score = _score + score
+    add_score((text + "\t%s") % score)
+
+
+# Function to replace text within a Google Doc
+def replace_text(old_text, new_text):
+    requests = []
+    text_range = {
+        'startIndex': 0,
+        'endIndex': -1
+    }
+    match_criteria = {
+        "text": old_text,
+        "matchCase": True
+
+    }
+    replace_all_text = {
+        'replaceAllText': {
+            'contains_text': match_criteria,
+            'replaceText': new_text
+        }
+    }
+    requests.append(replace_all_text)
+    result = docs_service.documents().batchUpdate(documentId=doc_id, body={'requests': requests}).execute()
+    return result
+
+def time_elapsed_in_s():
+    return datetime.now() - _start_time
 
 def on_started():
-    # Copy document
-
-    # Open document in Chrome
-
-    # Fill out date
-    pass
-
+    global _start_time
+    replace_text("#DATUM", datetime.today().strftime('%Y-%m-%d'))
     # Start timer
+    _start_time = datetime.today()
+    # Add to log
+    add_to_log(MISSION_STARTED[_language] % _start_time.strftime('%Y-%m-%d %H:%M:%S'))
+    add_to_log(ENTERED_SPACESHIP[_language] % (_start_time + timedelta(seconds=11)).strftime('%Y-%m-%d %H:%M:%S'))
+    
 
 def on_reset():
     # Reset timer
@@ -53,10 +144,28 @@ def on_stopped():
 def on_language_change(language):
     global _language
     _language = language
+    print("Switching language to: %s" % language)
+    text_de = "Missions-Report: Das verlassene Raumschiff"
+    text_en = "Mission report: The abandoned spaceship"
+    if language == "english":
+        replace_text(text_de, text_en)
+    if language == "deutsch":
+        replace_text(text_en, text_de)
 
 def on_event(event):
-    pass
+    if event == "Dock door unlocked":
+        add_to_log_with_time(DOCK_DOOR_UNLOCKED[_language])
+        t = time_elapsed_in_s()
+        if t.seconds < 300:
+            add_to_score(100, INTRUDER_ALERT_SUCCESS[_language])
+            if t.seconds < 180:
+                add_to_score(180 - t.seconds, QUICKNESS_BONUS[_language])
+        else:
+            add_to_score(-10, INTRUDER_ALERT_FAILED[_language])
 
+    if event == "Dock keypad solved":
+        add_to_log_with_time(DOCK_KEYPAD_SOLVED[_language])
+                
 
 ### Google Login ###
 def login():
@@ -87,35 +196,14 @@ def login():
 def copy_doc(service, document_id, new_title):
     body = {
         'name': "Das verlassene Raumschiff",
-        'parents': [FOLDER_ID]  # You can specify a parent folder ID here
+        'parents': [FOLDER_ID]  
     }
     request = service.files().copy(fileId=document_id, body=body)
-    response = request.execute()
-    copy_id = response.get('id')
+    #response = request.execute()
+    #copy_id = response.get('id')
+    copy_id = "1Glfl9MouxVsf3KHSWUyUblfx4OhTwmCHMgJwSHPVy9A"
     print("New doc created: %s" % copy_id)
     return copy_id
-
-# Function to replace text within a Google Doc
-def replace_text(service, document_id, old_text, new_text):
-    requests = []
-    text_range = {
-        'startIndex': 0,
-        'endIndex': -1
-    }
-    match_criteria = {
-        "text": old_text,
-        "matchCase": True
-
-    }
-    replace_all_text = {
-        'replaceAllText': {
-            'contains_text': match_criteria,
-            'replaceText': new_text
-        }
-    }
-    requests.append(replace_all_text)
-    result = service.documents().batchUpdate(documentId=document_id, body={'requests': requests}).execute()
-    return result
 
 def find_all_text(docs_service, document_id, search_text):
     request = {
@@ -176,33 +264,36 @@ def connect(client):
             print('An exception occured: {}'.format(e))
             sleep(5)
 
-#mqttc = mqtt.Client()
-#mqttc.on_connect = on_connect
-#mqttc.on_message = on_message
-#mqttc.username_pw_set(username="outpost", password="CallingHome")
+mqttc = mqtt.Client()
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.username_pw_set(username="outpost", password="CallingHome")
 
-#connect(mqttc)
+connect(mqttc)
 
 print("starting message queue.")
-#mqttc.loop_start()
+mqttc.loop_start()
 
 def main():
+    global docs_service
+    global doc_id
+
     creds = login()
     drive_service = build("drive", "v3", credentials=creds)
     docs_service = build("docs", "v1", credentials=creds)
     new_doc_title = 'Copied Document'
 
     # Copy the original document
-    new_doc_id = copy_doc(drive_service, DOCUMENT_ID, new_doc_title)
+    doc_id = copy_doc(drive_service, DOCUMENT_ID, new_doc_title)
 
     # Replace "#TEXT" with "Hello world" in the copied document
-    replace_text(docs_service, new_doc_id, 'as', 'ASS')
-    #for match in find_all_text(docs_service, new_doc_id, "as"):
-    #    print(match["range"])
+    # replace_text(docs_service, new_doc_id, 'as', 'ASS')
 
-    print(f"Document copied successfully. New document ID: {new_doc_id}")
-
-
+    print(f"Document copied successfully. New document ID: {doc_id}")
+    webbrowser.open("https://docs.google.com/document/d/%s" % doc_id)
+    
+    while True:
+        sleep(1)
 
 
     # Retrieve the documents contents from the Docs service.
